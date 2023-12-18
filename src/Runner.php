68891612a4453bfbda6379d561f3c65a2afa0c2c
= <=>
2023-12-18 16:18:47 +0200

    Add missing features

--- src/Runner.php
+++ src/Runner.php
@@ -54,10 +54,7 @@ class Runner
 
             if ($class instanceof ClassType) {
 
-                foreach ($phpFile->getNamespaces() as $namespace) {
-                    $namespace->addUse('Doctrine\ORM\Mapping', 'ORM');
-                    $namespace->addUse('Doctrine\DBAL\Types\Types');
-                }
+                $this->addUseStatement($phpFile, 'Doctrine\ORM\Mapping', 'ORM');
 
                 $this->handleEntityAttribute($yamlEntity, $phpFile, $class);
                 if ($yamlEntity['type'] === 'entity') {
@@ -66,6 +63,55 @@ class Runner
                 unset($yamlEntity['repositoryClass']);
                 $this->handleTableAttribute($yamlEntity, $class);
                 unset($yamlEntity['table']);
+
+                if (array_key_exists('uniqueConstraints', $yamlEntity)) {
+                    foreach ($yamlEntity['uniqueConstraints'] as $key => $field) {
+                        $this->handleUniqueConstraint($class, $key, $field);
+                    }
+                    unset($yamlEntity['uniqueConstraints']);
+                }
+
+                if (array_key_exists('indexes', $yamlEntity)) {
+                    foreach ($yamlEntity['indexes'] as $key => $field) {
+                        $this->handleIndex($class, $key, $field);
+                    }
+                    unset($yamlEntity['indexes']);
+                }
+
+                if (array_key_exists('entityListeners', $yamlEntity)) {
+                    $attributes = [];
+                    $attributes[] = $yamlEntity['entityListeners'];
+
+                    $class->addAttribute('Doctrine\ORM\Mapping\EntityListeners', $attributes);
+
+                    unset($yamlEntity['entityListeners']);
+                }
+
+                if (array_key_exists('inheritanceType', $yamlEntity)) {
+                    $attributes = [];
+                    $attributes[] = $yamlEntity['inheritanceType'];
+
+                    $class->addAttribute('Doctrine\ORM\Mapping\InheritanceType', $attributes);
+
+                    unset($yamlEntity['inheritanceType']);
+                }
+
+                if (array_key_exists('discriminatorColumn', $yamlEntity)) {
+                    $attributes = $yamlEntity['discriminatorColumn'];
+
+                    $class->addAttribute('Doctrine\ORM\Mapping\DiscriminatorColumn', $attributes);
+
+                    unset($yamlEntity['discriminatorColumn']);
+                }
+
+                if (array_key_exists('discriminatorMap', $yamlEntity)) {
+                    $attributes = [];
+                    $attributes[] = $yamlEntity['discriminatorMap'];
+
+                    $class->addAttribute('Doctrine\ORM\Mapping\DiscriminatorMap', $attributes);
+
+                    unset($yamlEntity['discriminatorMap']);
+                }
 
                 if (array_key_exists('id', $yamlEntity)) {
                     foreach ($yamlEntity['id'] as $key => $field) {
@@ -81,8 +127,40 @@ class Runner
                     unset($yamlEntity['fields']);
                 }
 
+                if (array_key_exists('oneToOne', $yamlEntity)) {
+                    foreach ($yamlEntity['oneToOne'] as $key => $field) {
+                        $this->handleOneToOne($phpFile, $class, $key, $field);
+                    }
+                    unset($yamlEntity['oneToOne']);
+                }
+
+                if (array_key_exists('manyToOne', $yamlEntity)) {
+                    foreach ($yamlEntity['manyToOne'] as $key => $field) {
+                        $this->handleManyToOne($phpFile, $class, $key, $field);
+                    }
+                    unset($yamlEntity['manyToOne']);
+                }
+
+                if (array_key_exists('oneToMany', $yamlEntity)) {
+                    foreach ($yamlEntity['oneToMany'] as $key => $field) {
+                        $this->handleOneToMany($phpFile, $class, $key, $field);
+                    }
+                    unset($yamlEntity['oneToMany']);
+                }
+
+                if (array_key_exists('gedmo', $yamlEntity)) {
+                    if (array_key_exists('soft_deleteable', $yamlEntity['gedmo'])) {
+                        $attributes = [
+                            'fieldName' => $yamlEntity['gedmo']['soft_deleteable']['field_name'],
+                            'timeAware' => $yamlEntity['gedmo']['soft_deleteable']['time_aware'],
+                        ];
+                        $class->addAttribute('Gedmo\Mapping\Annotation\SoftDeleteable', $attributes);
+                    }
+                    unset($yamlEntity['gedmo']);
+                }
+
                 if (sizeof($yamlEntity) > 0) {
-                    dump('Unsupported property key: ', $yamlEntity); die;
+                    dump('Unsupported class key: ', $yamlEntity);
                 }
 
                 $printer = new PsrPrinter();
@@ -153,7 +231,10 @@ class Runner
             if (isset($field['type'])) {
                 $type = $this->convertType($field['type']);
                 if ($type) {
+                    $this->addUseStatement($phpFile, 'Doctrine\DBAL\Types\Types');
                     $columnAttributes['type'] = new Literal('Types::' . $this->convertType($field['type']));
+                } else {
+                    $columnAttributes['type'] = $field['type'];
                 }
                 unset($field['type']);
             }
@@ -184,34 +265,237 @@ class Runner
             }
 
             if (sizeof($field) > 0) {
-                dump('Unsupported property key: ', $field); die;
+                dump('Unsupported column key: ', $field); die;
             }
 
             $property->addAttribute('Doctrine\ORM\Mapping\Column', $columnAttributes);
         }
     }
 
+    public function handleOneToOne(PhpFile $phpFile, ClassType $class, int|string $key, array $field): void
+    {
+        if ($class->hasProperty($key)) {
+            $property = $class->getProperty($key);
+
+            $attributes = [];
+
+            if (isset($field['targetEntity'])) {
+                $attributes['targetEntity'] = new Literal($field['targetEntity'] . '::class');
+                unset($field['targetEntity']);
+            }
+
+            if (isset($field['mappedBy'])) {
+                $attributes['mappedBy'] = $field['mappedBy'];
+                unset($field['mappedBy']);
+            }
+
+            if (isset($field['cascade'])) {
+                $attributes['cascade'] = $field['cascade'];
+                unset($field['cascade']);
+            }
+
+            if (isset($field['fetch'])) {
+                $attributes['fetch'] = $field['fetch'];
+                unset($field['fetch']);
+            }
+
+            if (isset($field['inversedBy'])) {
+                $attributes['inversedBy'] = $field['inversedBy'];
+                unset($field['inversedBy']);
+            }
+
+            if (isset($field['joinColumn'])) {
+                $joinColumnAttributes = $field['joinColumn'];
+                unset($field['joinColumn']);
+            }
+
+            if (sizeof($field) > 0) {
+                dump('Unsupported OneToOne key: ', $field); die;
+            }
+
+            $property->addAttribute('Doctrine\ORM\Mapping\OneToOne', $attributes);
+
+            if (isset($joinColumnAttributes)) {
+                $property->addAttribute('Doctrine\ORM\Mapping\JoinColumn', $joinColumnAttributes);
+            }
+        }
+    }
+
+    public function handleManyToOne(PhpFile $phpFile, ClassType $class, int|string $key, array $field): void
+    {
+        if ($class->hasProperty($key)) {
+            $property = $class->getProperty($key);
+
+            $attributes = [];
+
+            if (isset($field['targetEntity'])) {
+                $attributes['targetEntity'] = new Literal($field['targetEntity'] . '::class');
+                unset($field['targetEntity']);
+            }
+
+            if (isset($field['cascade'])) {
+                $attributes['cascade'] = $field['cascade'];
+                unset($field['cascade']);
+            }
+
+            if (isset($field['fetch'])) {
+                $attributes['fetch'] = $field['fetch'];
+                unset($field['fetch']);
+            }
+
+            if (isset($field['inversedBy'])) {
+                $attributes['inversedBy'] = $field['inversedBy'];
+                unset($field['inversedBy']);
+            }
+
+            if (isset($field['joinColumn'])) {
+                $joinColumnAttributes = $field['joinColumn'];
+                unset($field['joinColumn']);
+            }
+
+            if (sizeof($field) > 0) {
+                dump('Unsupported ManyToOne key: ', $field); die;
+            }
+
+            $property->addAttribute('Doctrine\ORM\Mapping\ManyToOne', $attributes);
+
+            if (isset($joinColumnAttributes)) {
+                $property->addAttribute('Doctrine\ORM\Mapping\JoinColumn', $joinColumnAttributes);
+            }
+        }
+    }
+
+    public function handleOneToMany(PhpFile $phpFile, ClassType $class, int|string $key, array $field): void
+    {
+        if ($class->hasProperty($key)) {
+            $property = $class->getProperty($key);
+
+            $attributes = [];
+
+            if (isset($field['mappedBy'])) {
+                $attributes['mappedBy'] = $field['mappedBy'];
+                unset($field['mappedBy']);
+            }
+
+            if (isset($field['targetEntity'])) {
+                $attributes['targetEntity'] = new Literal($field['targetEntity'] . '::class');
+                unset($field['targetEntity']);
+            }
+
+            if (isset($field['cascade'])) {
+                $attributes['cascade'] = $field['cascade'];
+                unset($field['cascade']);
+            }
+
+            if (isset($field['fetch'])) {
+                $attributes['fetch'] = $field['fetch'];
+                unset($field['fetch']);
+            }
+
+            if (sizeof($field) > 0) {
+                dump('Unsupported OneToMany key: ', $field); die;
+            }
+
+            $property->addAttribute('Doctrine\ORM\Mapping\OneToMany', $attributes);
+        }
+    }
+
     private function convertType(string $type): ?string
     {
-        return match ($type) {
-            'ascii_string' => 'ASCII_STRING',
-            'bigint' => 'BIGINT',
-            'binary' => 'BINARY',
-            'blob' => 'BLOB',
-            'boolean' => 'BOOLEAN',
-            'date' => 'DATE_MUTABLE',
-            'date_immutable' => 'DATE_IMMUTABLE',
-            'dateinterval' => 'DATEINTERVAL',
-            'datetime' => 'DATETIME_MUTABLE',
-            'datetime_immutable' => 'DATETIME_IMMUTABLE',
-            'datetimetz' => 'DATETIMETZ_MUTABLE',
-            'datetimetz_immutable' => 'DATETIMETZ_IMMUTABLE',
-            'decimal' => 'DECIMAL',
-            'float' => 'FLOAT',
-            'guid' => 'GUID',
-            'integer' => 'INTEGER',
-            'json' => 'JSON',
-            default => null,
-        };
+        switch ($type) {
+            case 'array':
+                return 'ARRAY';
+            case 'ascii_string':
+                return 'ASCII_STRING';
+            case 'bigint':
+                return 'BIGINT';
+            case 'binary':
+                return 'BINARY';
+            case 'blob':
+                return 'BLOB';
+            case 'boolean':
+                return 'BOOLEAN';
+            case 'date':
+                return 'DATE_MUTABLE';
+            case 'date_immutable':
+                return 'DATE_IMMUTABLE';
+            case 'dateinterval':
+                return 'DATEINTERVAL';
+            case 'datetime':
+                return 'DATETIME_MUTABLE';
+            case 'datetime_immutable':
+                return 'DATETIME_IMMUTABLE';
+            case 'datetimetz':
+                return 'DATETIMETZ_MUTABLE';
+            case 'datetimetz_immutable':
+                return 'DATETIMETZ_IMMUTABLE';
+            case 'decimal':
+                return 'DECIMAL';
+            case 'float':
+                return 'FLOAT';
+            case 'guid':
+                return 'GUID';
+            case 'integer':
+                return 'INTEGER';
+            case 'json':
+                return 'JSON';
+            case 'object':
+                return 'OBJECT';
+            case 'simple_array':
+                return 'SIMPLE_ARRAY';
+            case 'smallint':
+                return 'SMALLINT';
+            case 'string':
+                return 'STRING';
+            case 'text':
+                return 'TEXT';
+            case 'time':
+                return 'TIME_MUTABLE';
+            case 'time_immutable':
+                return 'TIME_IMMUTABLE';
+            case 'uuid':
+                return null;
+        }
+
+        dump('Unrecognized property type', $type); die;
+    }
+
+    private function handleUniqueConstraint(ClassType $class, int|string $key, array $field): void
+    {
+        $attributes = [
+            'name' => $key,
+            'columns' => $field['columns']
+        ];
+
+        unset($field['columns']);
+
+        if (sizeof($field) > 0) {
+            dump('Unsupported UniqueConstraint key: ', $field); die;
+        }
+
+        $class->addAttribute('Doctrine\ORM\Mapping\UniqueConstraint', $attributes);
+    }
+
+    private function handleIndex(ClassType $class, int|string $key, array $field): void
+    {
+        $attributes = [
+            'name' => $key,
+            'columns' => $field['columns']
+        ];
+
+        unset($field['columns']);
+
+        if (sizeof($field) > 0) {
+            dump('Unsupported Index key: ', $field); die;
+        }
+
+        $class->addAttribute('Doctrine\ORM\Mapping\Index', $attributes);
+    }
+
+    private function addUseStatement(PhpFile $phpFile, string $name, ?string $alias = null)
+    {
+        foreach ($phpFile->getNamespaces() as $namespace) {
+            $namespace->addUse($name, $alias);
+        }
     }
 }
